346aaa5ef8e348aa04ddaf8ad11798ca5835cb1f
aakash1612 <varshneyakash46@gmail.com>
2026-03-10 14:46:42 +0530

    update

--- Front-end/src/socket/socket.js
+++ Front-end/src/socket/socket.js
@@ -1,7 +1,9 @@
 import { io } from "socket.io-client";
 
 const socket = io(
-  import.meta.env.VITE_SERVER_URL || "http://localhost:5000"
+  import.meta.env.VITE_SERVER_URL || "http://localhost:5000",{
+    transports: ["websocket"]
+  }
 );
 
 export default socket;
